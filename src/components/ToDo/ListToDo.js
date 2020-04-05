7673f007299a64f099ab5c5076b7bdfbb8d1ed2a
sanjaysaravanan <sanjaysaravanan38@gmail.com>
2020-04-05 18:56:37 +0530

    delete, update, add feature added in ToDo List

--- src/components/ToDo/ListToDo.js
+++ src/components/ToDo/ListToDo.js
@@ -1,21 +1,46 @@
 import React, { useState, useEffect } from "react";
-import { retrieveAllTodos } from "../../api/todo/ToDoDataService";
+import { retrieveAllTodos, deleteTodo } from "../../api/todo/ToDoDataService";
 import { getLoggedInUser } from "./AuthenticationService";
+import { Link, useHistory } from "react-router-dom";
+import moment from "moment";
 
 function ListToDo() {
 	const [todos, setTodos] = useState([]);
+	const [message, setMessage] = useState(null);
+
+	const history = useHistory();
 
 	useEffect(() => {
+		refreshTodos();
+	}, []);
+
+	const refreshTodos = () => {
 		let userName = getLoggedInUser();
-		console.log(userName);
 		retrieveAllTodos(userName)
 			.then(res => setTodos(res.data))
 			.catch(error => console.log(error));
-	}, []);
+	};
+
+	const deleteTodoClicked = id => {
+		let userName = getLoggedInUser();
+		deleteTodo(userName, id).then(response => {
+			setMessage(`Deletion of todo ${id} Successfull`);
+			refreshTodos();
+		});
+	};
+
+	const addTodoClicked = () => {
+		history.push(`/todos/${-1}`);
+	};
+
+	const updateTodoClicked = id => {
+		console.log("Update " + id);
+	};
 
 	return (
 		<div>
 			<h1>ToDo List</h1>
+			{message && <div className="alert alert-success">{message}</div>}
 			<div className="container">
 				<table className="table">
 					<thead>
@@ -23,18 +48,43 @@ function ListToDo() {
 							<th>Description</th>
 							<th>Target Date</th>
 							<th>Completed</th>
+							<th>Update</th>
+							<th>Delete</th>
 						</tr>
 					</thead>
 					<tbody>
 						{todos.map(todo => (
 							<tr key={todo.id}>
 								<td>{todo.description}</td>
-								<td>{todo.targetDate.toString()}</td>
+								<td>{moment(todo.targetDate).format("YYYY-MM-DD")}</td>
 								<td>{todo.done.toString()}</td>
+								<td>
+									<Link to={`/todos/${todo.id}`}>
+										<button
+											className="btn btn-success"
+											onClick={() => updateTodoClicked(todo.id)}
+										>
+											Update
+										</button>
+									</Link>
+								</td>
+								<td>
+									<button
+										className="btn btn-warning"
+										onClick={() => deleteTodoClicked(todo.id)}
+									>
+										Delete
+									</button>
+								</td>
 							</tr>
 						))}
 					</tbody>
 				</table>
+				<div className="row">
+					<button className="btn btn-success" onClick={() => addTodoClicked()}>
+						Add
+					</button>
+				</div>
 			</div>
 		</div>
 	);
---
+++ src/components/ToDo/ToDoComponent.js
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from "react";
+import moment from "moment";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { useHistory } from "react-router-dom";
+
+import {
+	getTodoById,
+	updateTodo,
+	createTodo
+} from "../../api/todo/ToDoDataService";
+import { getLoggedInUser } from "./AuthenticationService";
+
+export default function ToDoComponent(props) {
+	const [id, setId] = useState(props.match.params.id);
+	const [description, setDescription] = useState("Learn Forms");
+	const [targetDate, setTargetDate] = useState(
+		moment(new Date()).format("YYYY-MM-DD")
+	);
+	const [isCompleted, setIsCompleted] = useState(false);
+	const history = useHistory();
+
+	const setStateFromApi = data => {
+		setId(data.id);
+		setDescription(data.description);
+		setTargetDate(moment(data.targetDate).format("YYYY-MM-DD"));
+		setIsCompleted(data.isCompleted);
+	};
+
+	useEffect(() => {
+		let userName = getLoggedInUser();
+		if (id === "-1") {
+			return;
+		} else getTodoById(userName, id).then(res => setStateFromApi(res.data));
+	}, [id]);
+
+	const todo = (values, userName) => {
+		return {
+			id: id,
+			username: userName,
+			description: values.description,
+			targetDate: values.targetDate,
+			done: values.isCompleted
+		};
+	};
+
+	const onSubmit = values => {
+		let userName = getLoggedInUser();
+		if (id === "-1") {
+			createTodo(userName, todo(values, userName));
+		} else updateTodo(userName, id, todo(values, userName));
+		history.push("/todos");
+	};
+
+	const validate = values => {
+		let errors = {};
+		if (!values.description) {
+			errors.description = "Enter a Description";
+		} else if (values.description.length < 5) {
+			errors.description = "Enter atlest 5 Characters in Description";
+		}
+
+		if (!moment(values.targetDate).isValid()) {
+			errors.targetDate = "Enter a valid Target Date";
+		}
+
+		return errors;
+	};
+
+	return (
+		<div>
+			<h1>ToDo</h1>
+			<div className="container">
+				<Formik
+					initialValues={{
+						description,
+						targetDate,
+						isCompleted
+					}}
+					onSubmit={onSubmit}
+					validateOnChange={false}
+					validateOnBlur={false}
+					validate={validate}
+					enableReinitialize={true}
+				>
+					{props => (
+						<Form>
+							<ErrorMessage
+								name="description"
+								component="div"
+								className="alert alert-warning"
+							/>
+							<ErrorMessage
+								name="targetDate"
+								component="div"
+								className="alert alert-warning"
+							/>
+							<fieldset className="form-group">
+								<label>Description</label>
+								<Field
+									className="form-control"
+									type="text"
+									name="description"
+								/>
+							</fieldset>
+							<fieldset className="form-group">
+								<label>Target Date</label>
+								<Field className="form-control" type="date" name="targetDate" />
+							</fieldset>
+							<fieldset className="form-group">
+								<label>Completed</label>
+								<Field
+									className="form-control"
+									type="checkbox"
+									name="isCompleted"
+								/>
+							</fieldset>
+							<button className="btn btn-success" type="submit">
+								Save
+							</button>
+						</Form>
+					)}
+				</Formik>
+			</div>
+		</div>
+	);
+}
